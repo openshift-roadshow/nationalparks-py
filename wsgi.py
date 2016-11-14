a381de108aa6ff5dd3b4c4b3ea0d43a78ed994c6
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-11-14 14:18:52 +1100

    Initial files.

---
+++ wsgi.py
@@ -0,0 +1,117 @@
+from __future__ import print_function
+
+import os
+import json
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+from pymongo import MongoClient, GEO2D
+
+DB_HOST = os.environ.get('DB_HOST', 'mongodb')
+DB_NAME = os.environ.get('DB_NAME', 'mongodb')
+
+DB_USERNAME = os.environ.get('DB_USERNAME', 'mongodb')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'mongodb')
+
+DB_URI = 'mongodb://%s:%s@%s:27017/%s' % (DB_USERNAME, DB_PASSWORD,
+        DB_HOST, DB_NAME)
+
+DATASET_FILE = 'nationalparks.json'
+
+application = Flask(__name__)
+
+api = Api(application)
+
+class HealthCheck(Resource):
+    def get(self):
+        return 'OK'
+
+api.add_resource(HealthCheck, '/ws/healthz/')
+
+class Info(Resource):
+    description = {
+        'id': 'nationalparks-py',
+        'displayName': 'National Parks (PY)',
+        'type': 'marker',
+        'center': {'latitude': '39.82', 'longitude': '-98.57'},
+        'zoom': 5
+    }
+
+    def get(self):
+        return self.description
+
+api.add_resource(Info, '/ws/info/')
+
+class DataLoad(Resource):
+    def get(self):
+        client = MongoClient(DB_URI)
+        database = client[DB_NAME]
+        collection = database.nationalparks
+
+        collection.remove({})
+        collection.create_index([('Location', GEO2D)])
+
+        with open(DATASET_FILE, 'rb') as fp:
+            entries = []
+
+            for data in fp.readlines():
+                entry = json.loads(data)
+
+                loc = [entry['coordinates'][1], entry['coordinates'][0]]
+                entry['Location'] = loc
+
+                entries.append(entry)
+
+                if len(entries) >= 1000:
+                    collection.insert_many(entries)
+                    entries = []
+
+            if entries:
+                collection.insert_many(entries)
+
+        return 'Inserted %s items.' % collection.count()
+
+api.add_resource(DataLoad, '/ws/data/load')
+
+def format_result(entries):
+    result = []
+
+    for entry in entries:
+        data = {}
+
+        data['id'] = entry['name']
+        data['latitude'] = str(entry['coordinates'][0])
+        data['longitude'] = str(entry['coordinates'][1])
+        data['name'] = entry['toponymName']
+
+        result.append(data)
+
+    return result
+
+class DataAll(Resource):
+    def get(self):
+        client = MongoClient(DB_URI)
+        database = client[DB_NAME]
+        collection = database.nationalparks
+
+        return format_result(collection.find())
+
+api.add_resource(DataAll, '/ws/data/all')
+
+class DataWithin(Resource):
+    def get(self):
+        args = request.args
+
+        box = [[float(args['lon1']), float(args['lat1'])],
+               [float(args['lon2']), float(args['lat2'])]]
+
+        query = {'Location': {'$within': {'$box': box}}}
+
+        client = MongoClient(DB_URI)
+        database = client[DB_NAME]
+        collection = database.nationalparks
+
+        return format_result(collection.find(query))
+
+api.add_resource(DataWithin, '/ws/data/within')
